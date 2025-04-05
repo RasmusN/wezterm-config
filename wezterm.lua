f155cfc18836f43bc1824102bb32c2354983d171
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2025-04-05 10:32:35 +0200

    adjust for MAC

--- wezterm.lua
+++ wezterm.lua
@@ -2,23 +2,26 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
-config.default_prog = { "bash" }
+config.use_dead_keys = false
 config.keys = {
+	{ key = "¨", mods = "OPT", action = wezterm.action.SendString('~') }, -- Needed for swedish_no_deadkeys
+	{ key = "2", mods = "OPT", action = wezterm.action.SendString('@') }, -- Needed for some reason
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action.SendKey({ key='b', mods='ALT'}) }, -- For jumping one word in the terminal input
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action.SendKey({ key='f', mods='ALT'}) },
 	{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
-	{ key = "h", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
-	{ key = "l", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "h", mods = "CMD", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "l", mods = "CMD", action = wezterm.action.ActivateTabRelative(1) },
 	{ key = "'", mods = "CTRL", action = wezterm.action.SendString("\x1b[27;5;39~") },
 	{ key = "+", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "r", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "Tab", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
-	{ key = "s", mods = "CTRL|ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "v", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "l", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Right") },
-	{ key = "k", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ key = "j", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ key = "q", mods = "CTRL|ALT", action = wezterm.action.CloseCurrentPane { confirm = false }},
+	{ key = "s", mods = "CTRL|CMD", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "CTRL|CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "q", mods = "CTRL|CMD", action = wezterm.action.CloseCurrentPane { confirm = false }},
 }
-
 return config
