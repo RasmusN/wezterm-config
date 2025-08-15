68192f2edcf0557914627ff68dec929a41e9c47b
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2025-08-15 09:43:02 +0200

    bind shift|cmd h/l to move tab

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,7 @@ config.color_scheme = "Tokyo Night"
 config.font_size = 14.0
 config.use_dead_keys = false
 config.keys = {
-    { key = "7", mods = "OPT", action = wezterm.action.SendString("|") },
+    { key = "7", mods = "OPT", action = wezterm.action.SendString("|") }, -- Needed for some reason
 	{ key = "¨", mods = "OPT", action = wezterm.action.SendString('~') }, -- Needed for swedish_no_deadkeys
 	{ key = "2", mods = "OPT", action = wezterm.action.SendString('@') }, -- Needed for some reason
 	{ key = "LeftArrow", mods = "OPT", action = wezterm.action.SendKey({ key='b', mods='ALT'}) }, -- For jumping one word in the terminal input
@@ -25,5 +25,8 @@ config.keys = {
 	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
 	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
 	{ key = "q", mods = "CTRL|CMD", action = wezterm.action.CloseCurrentPane { confirm = false }},
+	{ key = "h", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(-1),},
+	{ key = "l", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(1),},
+
 }
 return config
